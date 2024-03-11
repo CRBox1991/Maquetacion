dca074b2051f82d96d7bb81297e8de8ae8973881
CRBox <dharianfenix@gmail.com>
2024-03-11 11:38:55 +0100

    .

--- maquetacion basica/agencia.js
+++ maquetacion basica/agencia.js
@@ -8,8 +8,7 @@ function info(){
     let destino = document.getElementById('destino').value;
     let personas = document.getElementById('personas').value;
     let fecha = document.getElementById('fecha').value;
-    console.log(nombre, origen, destino, personas, fecha);
-    
+    console.log(nombre, origen, destino, personas, fecha);  
 
     let compilation = {
         nombre : nombre,
@@ -18,16 +17,22 @@ function info(){
         personas :personas,
         fecha : fecha,
     }
+    myArrInfo.push(compilation)    
 }
-    myArrInfo.push(compilation)
-    //console.log(myArrInfo);
+    
+    console.log(myArrInfo);
 
-    let infor = document.getElementById('compilation')
 
 function filter()
-{
-    for(let i = 0; i < myArrInfo.length; i++){
-        
-    infor.innerHTML[i] += `<p>${myArrInfo[i]}</p>` 
-    }                       
+{  
+   info.innerHTML = "" 
+    for (let i=0; i < myArrInfo.length; i++) 
+    {    
+        let info = document.getElementById("compilation")
+        info.innerHTML += "<h3>" + myArrInfo[i].nombre + "</h3>" +
+             "<p>" + myArrInfo[i].origen + "</p>" +
+             "<p>" + myArrInfo[i].destino + "</p>" +
+             "<p>" + myArrInfo[i].numero + "</p>" +
+             "<p>" + myArrInfo[i].fecha + "</p>"; 
+}
 }
